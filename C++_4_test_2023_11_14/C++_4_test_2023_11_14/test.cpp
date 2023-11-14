6800b1c68b22ba1cd19c7c2623b1b6c263cb0fb5
ecouseray <ecouseray@gmail.com>
2023-11-14 21:07:01 +0800

    test_2023_11_14_C++_4.1

---
+++ C++_4_test_2023_11_14/C++_4_test_2023_11_14/test.cpp
@@ -0,0 +1,88 @@
+// 6个默认成员函数
+// 
+
+/*
+#include <iostream>
+using namespace std;
+
+class Data
+{
+public:
+	Data(int year = 0, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Data()" << ":" << this << endl;
+	}
+	~Data() // 清理资源的(完成清理工作)
+	{
+		cout << "~Data()" << ":" <<this <<endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+class Stack
+{
+public:
+	// 对象创建时候自动调用，完成初始化工作
+	Stack(int n = 10)
+	{
+		_a = (int*)malloc(sizeof(int) * n);
+		_size = 0;
+		_capacity = n;
+	}
+	// 对象生命周期到了以后，完成清理工作
+	~Stack()
+	{
+		free(_a);
+		_a = nullptr;
+		_size = _capacity = 0;
+	}
+
+	void Push(int x)
+	{
+
+	}
+	void Pop()
+	{
+
+	}
+	size_t Size()
+	{
+
+	}
+
+private:
+	int* _a;
+	int _size;
+	int _capacity;
+};
+
+int main()
+{
+	//Data d1; // 1.我们不写编译器自动生成 2.全缺省 3.无参的（三必一） -- 默认构造函数 -- 不传参就可以调用的
+	// Data d1(&d1); -- this 指针
+	//Data d2;
+	// d1 先构造，d2 后构造，d2 先析构，d1 后析构
+	// 生命周期结束自动调用，栈空间，先进后出
+
+	Stack st;
+
+	// 构造函数，是完成初始化的工作，如果类嵌套的话，默认类型不会初始化，被嵌套的类会初始化
+	// 析构函数，是完成清理资源的工作，如果类嵌套的话，默认类型不会做什么，被嵌套的类会调用该类的析构函数
+
+	return 0;
+}
+
+*/
+
+// 构造函数，是完成初始化的工作，如果类嵌套的话，默认类型不会初始化，被嵌套的类会初始化
+// 析构函数，是完成清理资源的工作，如果类嵌套的话，默认类型不会做什么，被嵌套的类会调用该类的析构函数
+
+
+// 拷贝构造
