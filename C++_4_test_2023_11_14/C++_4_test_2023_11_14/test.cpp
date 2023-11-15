248a7d418895e3f60d571f1812e3ba907e41b6dc
ecouseray <ecouseray@gmail.com>
2023-11-15 20:53:45 +0800

    test_2023_11_15

--- C++_4_test_2023_11_14/C++_4_test_2023_11_14/test.cpp
+++ C++_4_test_2023_11_14/C++_4_test_2023_11_14/test.cpp
@@ -85,4 +85,107 @@ int main()
 // 析构函数，是完成清理资源的工作，如果类嵌套的话，默认类型不会做什么，被嵌套的类会调用该类的析构函数
 
 
+
+
 // 拷贝构造
+//#include <iostream>
+//using namespace std;
+//class Data
+//{
+//public:
+//	Data(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//		cout << "Data()" << ":" << this << endl;
+//	}
+//	
+//	// Data d2(d1);
+//	Data(const Data& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//		//d._day = _day;
+//	}
+//
+//private:
+//	int _year;
+//	int _month;
+//	int _day;
+//};
+//
+//void func(Data d)
+//{
+//}
+//
+//int main()
+//{
+//	Data d1(2023, 11, 15);
+//	//Data d2(2023, 11, 15);
+//	Data d2(d1);// 拷贝构造
+//	
+//	func(d2);
+//
+//	return 0;
+//}
+
+
+
+// 运算符的重载
+
+#include <iostream>
+using namespace std;
+class Data
+{
+public:
+	Data(int year = 0, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Data()" << ":" << this << endl;
+	}
+	
+	// Data d2(d1);
+	Data(const Data& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		//d._day = _day;
+	}
+
+//private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+//bool IsDataEqual(const Data& d1, const Data& d2)
+//{
+//	//...
+//}
+
+// 运算符有几个操作数，operater重载的函数就有几个参数
+bool operator==(const Data& d1, const Data& d2)
+{
+	return d1._year  == d2._year
+		&& d1._month == d2._month
+		&& d1._day   == d2._day;
+}
+
+int main()
+{
+	Data d1(2023, 11, 15);
+	Data d2(2023, 11, 16);
+	// 在这个地方，加入想要比较大小相等
+	d1 == d2; // operator==(d1, d2); 
+	// 一般不会这样写，因为可读性不好
+	//d1 > d2;
+	return 0;
+}
+
+
+// 虽然解决了问题，但是这里我们将访问限定符改变成了公有，但是将上不应该修改
