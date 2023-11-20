8463699b8f18a1944fec89efa5729d324cf2133c
ecouseray <ecouseray@gmail.com>
2023-11-20 21:40:28 +0800

    test_2023_11_20

--- C++_4_test_2023_11_14/C++_4_test_2023_11_14/test.cpp
+++ C++_4_test_2023_11_14/C++_4_test_2023_11_14/test.cpp
@@ -135,67 +135,272 @@ int main()
 
 // 运算符的重载
 
-#include <iostream>
-using namespace std;
-class Data
-{
-public:
-	Data(int year = 0, int month = 1, int day = 1)
-	{
-		_year = year;
-		_month = month;
-		_day = day;
-		cout << "Data()" << ":" << this << endl;
-	}
-	
-	// Data d2(d1);
-	Data(const Data& d)
-	{
-		_year = d._year;
-		_month = d._month;
-		_day = d._day;
-		//d._day = _day;
-	}
-
+//#include <iostream>
+//using namespace std;
+//class Data
+//{
+//public:
+//	Data(int year = 0, int month = 1, int day = 1)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//		cout << "Data()" << ":" << this << endl;
+//	}
+//	
+//	// Data d2(d1);
+//	Data(const Data& d)
+//	{
+//		_year = d._year;
+//		_month = d._month;
+//		_day = d._day;
+//		//d._day = _day;
+//	}
+//	// 运算符有几个操作数，operater重载的函数就有几个参数
+//	// operator==(&d1, d2)
+//	bool operator==(const Data& d)
+//	{
+//		return _year == d._year
+//			&& _month == d._month
+//			&& _day == d._day;
+//	}
+//
+//	// bool operator(Data* d1, const Data& d2)
+//	bool operator>(const Data& d)
+//	{
+//		if (this->_year > d._year)
+//			return true;
+//		if (this->_year == d._year)
+//			if (_month > d._month)
+//				return true;
+//		if (this->_year == d._year)
+//			if (_month = d._month)
+//				if (_day > d._day)
+//					return true;
+//
+//		return false;
+//	}
+//
+//
 //private:
-	int _year;
-	int _month;
-	int _day;
-};
+//	int _year;
+//	int _month;
+//	int _day;
+//};
 
 //bool IsDataEqual(const Data& d1, const Data& d2)
 //{
 //	//...
 //}
 
-// 运算符有几个操作数，operater重载的函数就有几个参数
-bool operator==(const Data& d1, const Data& d2)
-{
-	return d1._year  == d2._year
-		&& d1._month == d2._month
-		&& d1._day   == d2._day;
-}
-
-int main()
-{
-	Data d1(2023, 11, 15);
-	Data d2(2023, 11, 16);
-	// 在这个地方，加入想要比较大小相等
-	d1 == d2; // operator==(d1, d2); 
-	// 一般不会这样写，因为可读性不好
-	//d1 > d2;
-	return 0;
-}
+// 自定义类型是不能使用运算符的，要用就得实现重载函数，自定义类型用的使用等价于调用这个重载函数
+//int main()
+//{
+//	Data d1(2023, 11, 15);
+//	Data d2(2023, 11, 16);
+//	// 在这个地方，加入想要比较大小相等
+//	d1 == d2; // d1.operator==(d2);
+//	// operator==(d1, d2); 
+//	// 一般不会这样写，因为可读性不好
+//
+//	d1 > d2;  // d1.operator>(d2);
+//	return 0;
+//}
 
 
 // 虽然解决了问题，但是这里我们将访问限定符改变成了公有，但是将上不应该修改
 
 
+// 实现一个完善的日期类
+#include <iostream>
+using namespace std;
+
+class Data
+{
+public:
+	int GetMonthDay(int year, int month)
+	{
+		static int days[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0))
+			return days[2] + 1;
+
+		return days[month];
+	}
+
+	Data(int year = 0, int month = 1, int day = 1)
+	{
+		if (year >= 0
+			&& month >= 1 && month <= 12 && day >= 0
+			&& day <= GetMonthDay(year, month))
+		{
+			_year = year;
+			_month = month;
+			_day = day;
+		}
+		else
+		{
+			cout << "非法日期" << endl;
+		}
+	}
+
+	Data(const Data& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+	}
+
+	bool operator<(const Data& d)
+	{
+		if (_year < d._year)
+		{
+			return true;
+		}
+		else if (_year == d._year && _month < d._month)
+		{
+			return true;
+		}
+		else if (_year == d._year && _month == d._month && _day < d._day)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	bool operator==(const Data& d)
+	{
+		return _year == d._year
+			&& _month == d._month
+			&& _day == d._day;
+	}
+
+	// 复用的优势，内聚高，维护性强
+	bool operator<=(const Data& d)
+	{
+		// 复用上面的实现
+		return (*this < d) || (*this == d);
+	}
+
+	bool operator>(const Data& d)
+	{
+		return !(*this <= d);
+	}
+
+	bool operator>=(const Data& d)
+	{
+		return !(*this < d);
+	}
+
+	bool operator!=(const Data& d)
+	{
+		return !(*this == d);
+	}
+	void print()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+
+	Data operator+(int day)
+	{
+		Data ret = *this;
+		ret._day += day;
+		while (ret._day > GetMonthDay(ret._year, ret._month))
+		{
+			ret._day -= GetMonthDay(ret._year, ret._month);
+			ret._month++;
+			if (ret._month == 13)
+			{
+				ret._year++;
+				ret._month = 1;
+			}
+		}
+		return ret;
+	}
+	//Data operator+=(int day)
+	Data& operator+=(int day)
+	//因为这里的this是外面存在的，所以可以优化成Data&
+	{
+		_day += day;
+		while (_day > GetMonthDay(_year, _month))
+		{
+			_day -= GetMonthDay(_year, _month);
+			_month++;
+			if (_month == 13)
+			{
+				_year++;
+				_month = 1;
+			}
+		}
+		return *this;
+	}
+
+	Data& operator-=(int day)
+	{
+		_day -= day;
+		while (_day <= 0)
+		{
+			_month--;
+			_day += GetMonthDay(_year, _month);
+			
+			if (_month == 0)
+			{
+				_year--;
+				_month = 13;
+			}
+		}
+		return *this;
+	}
+
+	Data& operator++()
+	{
+		*this += 1;
+		return *this;
+	}
+	Data& operator--()
+	{
+		*this -= 1;
+		return *this;
+	}
+	int operator-(const Data& d)
+	{
+		if (*this < d)
+			return -1;
+		if (*this == d)
+			return 0;
+
+		int ret = 0;
+
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
 
 
+// 是否重载一个运算符，要看这个运算符是否对这个类的对象有意义
+int main()
+{
+	Data d1(2003, 1, 23);
+	d1.print();
+	Data d2(2023, 2, 28);
+	d2.print();
 
+	Data d3 = d2 + 10;
+	d3.print();
 
+	Data d5 = d1 + 30000;
+	d5.print();
 
+	Data d4(2003, 1, 1);
+	--d4;
+	d4.print();
+	return 0;
+}
 
 
 
